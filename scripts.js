a889bcdecb95b05471123e5aee6172c4255224e2
maxandart <62931168+maxandart@users.noreply.github.com>
2020-04-09 11:29:59 +0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,65 @@
+var countDiv = 10;
+var countImg = 5;
+var iActive = 0;
+var iActiveSlide = 0;
+var path = "../slider/page";
+var volImg = 10;
+var volSlider = 200;
+var num = 0;
+
+//режем картинку на квадратики создаём для каждого участка класс
+function CreateImg() {
+    for(var j = 0; j < countDiv; ++j){
+        for(var i = 0; i < countDiv; ++i){
+            var html = "<div class = 'img" + i + "'></div>";
+            var img = $(html);
+            img.css("background-position", "-" + i * 510 / countDiv + "px -" + j * 300 / countDiv + "px");
+            img.css("width", 510 / countDiv + "px");
+            img.css("height", 300 / countDiv + "px"); 
+            img.appendTo("#main");
+        }
+    }
+
+}
+
+//прорисовка новой картинки
+function ChangeImg(a) {
+
+        if (a != -1)
+            iActive = a;
+        var j = 1;
+        $("#main div").each(function() {
+            ++j;
+                $(this).fadeOut(volImg * j, function() {
+                $(this).css('background-image', 'url("' + path + iActive + '.jpg")');
+                $(this).fadeIn(volImg * j);
+            });
+        });
+
+}
+
+//при нажатии левой кнопки сменяеем изображение на левое
+function LeftImg(){
+    --iActive;
+    if (iActive < 0){
+        iActive = countImg - 1;
+    }
+    ChangeImg(-1);
+}
+
+//при нажатии правой кнопки сменяеем изображение на правое
+function RightImg(){
+    ++iActive;
+    if (iActive >= countImg){
+        iActive = 0;
+    }
+    ChangeImg(-1);
+}
+
+
+
+//вывзов функций
+$(document).ready(function() {
+    CreateImg();
+    ChangeImg(iActive);
+});
